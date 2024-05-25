2196a89e4647a86da64c940b27aa0943d4d4ed4f
Josuesp1620 <josuesp1620@gmail.com>
2024-05-25 09:59:48 -0500

    Add Contact Page

--- src/core/routes/allRoutes.js
+++ src/core/routes/allRoutes.js
@@ -1,9 +1,11 @@
 import IconList from "@/components/elements/IconGeneratos";
+import { ContactPage } from "@/contact";
 import { HomePage } from "@/home";
 
 const publicRoutes = [
   { path: "/", component: HomePage },
   { path: "/icons", component: IconList },
+  { path: "/contact", component: ContactPage },
 
 ]
 
