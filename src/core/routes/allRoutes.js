424144eacc1582068ff3c8f717ba3ba9807050da
Josuesp1620 <josuesp1620@gmail.com>
2024-05-25 18:56:05 -0500

    Add gallery Page

--- src/core/routes/allRoutes.js
+++ src/core/routes/allRoutes.js
@@ -1,12 +1,13 @@
 import IconList from "@/components/elements/IconGeneratos";
-import { ContactPage } from "@/contact";
-import { HomePage } from "@/home";
+import { ContactPage } from "@/pages/contact";
+import { GalleryPage } from "@/pages/gallery";
+import { HomePage } from "@/pages/home";
 
 const publicRoutes = [
   { path: "/", component: HomePage },
   { path: "/icons", component: IconList },
   { path: "/contact", component: ContactPage },
-
+  { path: "/gallery", component: GalleryPage },
 ]
 
 export { publicRoutes };
---
+++ src/pages/gallery/index.jsx
@@ -0,0 +1,20 @@
+import Layout from "@/components/layout/Layout"
+import GalleryFilter from "@/components/sections/gallery"
+
+export const GalleryPage = () => {
+    return (
+        <Layout breadcrumbTitle="Galeria">
+                
+            {/* portfolio-section */}
+            <section className="portfolio style__one two see__pad p_relative ">
+                <div className="auto_container">
+                <div className="sortable-masonry">
+                    <GalleryFilter />
+                </div>
+                </div>
+            </section>
+            {/* project-style-1 */}
+
+        </Layout>
+    )
+}
