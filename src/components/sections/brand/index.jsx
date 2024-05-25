3ffd493fa7a9e46bf8c496721e195002bed14d6c
Josuesp1620 <josuesp1620@gmail.com>
2024-05-25 18:58:32 -0500

    Refactor Component Brand

---
+++ src/components/sections/brand/index.jsx
@@ -0,0 +1,20 @@
+import './index.css'
+import BrandSlider1 from "../../slider/BrandSlider1";
+
+export default function Brand() {
+    return (
+        <>
+        {/* brand-section*/}
+        <section className="brand__section brand__one see__pad">
+            <div className="auto_container">
+            <div className="barnd___content">
+                <div className="medium-container">
+                    <BrandSlider1/>
+                </div>
+            </div>
+            </div>
+        </section>
+        {/* brand-section end */}
+        </>
+    )
+}
