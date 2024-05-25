d2b6c1024b2bff66666a3f92dbe53c6a191f386c
Josuesp1620 <josuesp1620@gmail.com>
2024-05-25 07:58:45 -0500

    Add Element WhatsApp Icon

---
+++ src/components/elements/WhastAppIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export const WhatsAppIcon = () => {
+    return (
+        <a href="https://wa.me/51999374434?text=Prueba" target="_blank" className="whatsapp-icon">
+            <i className="ri-whatsapp-line"></i>
+        </a>
+    );
+}
