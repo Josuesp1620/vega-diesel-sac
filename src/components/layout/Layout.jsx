b8b2df44735ecce2c20e7f7f460f6b781935d558
Josuesp1620 <josuesp1620@gmail.com>
2024-05-25 18:57:10 -0500

    Refactor Style Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,11 +2,11 @@
 import { useState } from "react"
 import BackToTop from '../elements/BackToTop'
 import DataBg from '../elements/DataBg'
-import Breadcrumb from './Breadcrumb'
-import SearchPopup from './SearchPopup'
+import Breadcrumb from './breadcrumb'
+import SearchPopup from './search'
 import Sidebar from './Sidebar'
-import Footer2 from './footer/Footer2'
-import Header4 from './header/Header4'
+import Footer2 from './footer'
+import Header4 from './header'
 import 'swiper/css'
 
 export default function Layout({ breadcrumbTitle, children, wrapperCls }) {
@@ -26,28 +26,13 @@ export default function Layout({ breadcrumbTitle, children, wrapperCls }) {
     const [isSidebar, setSidebar] = useState(false)
     const handleSidebar = () => setSidebar(!isSidebar)
 
-    // useEffect(() => {
-    //     const WOW = require('wowjs')
-    //     window.wow = new WOW.WOW({
-    //         live: false
-    //     })
-    //     window.wow.init()
-
-    //     document.addEventListener("scroll", () => {
-    //         const scrollCheck = window.scrollY > 100
-    //         if (scrollCheck !== scroll) {
-    //             setScroll(scrollCheck)
-    //         }
-    //     })
-    // }, [])
     return (
         <>
-            <DataBg />
             <div className={`boxed_wrapper ${wrapperCls ? wrapperCls : ""}`} id="#top">
                 <Header4 scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} handlePopup={handlePopup} isSidebar={isSidebar} handleSidebar={handleSidebar} />
 
 
-                <Sidebar isSidebar={isSidebar} handleSidebar={handleSidebar} />
+                {/* <Sidebar isSidebar={isSidebar} handleSidebar={handleSidebar} /> */}
                 <SearchPopup isPopup={isPopup} handlePopup={handlePopup} />
 
                 {breadcrumbTitle && <Breadcrumb breadcrumbTitle={breadcrumbTitle} />}
