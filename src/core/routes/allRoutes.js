295e8e8e93a7649a54e32b0fc1b93083d7dd6be9
Josuesp1620 <josuesp1620@gmail.com>
2024-05-26 08:00:27 -0500

    Add Route About

--- src/core/routes/allRoutes.js
+++ src/core/routes/allRoutes.js
@@ -1,4 +1,5 @@
 import IconList from "@/components/elements/IconGeneratos";
+import { AboutUsPage } from "@/pages/about_us";
 import { ContactPage } from "@/pages/contact";
 import { GalleryPage } from "@/pages/gallery";
 import { HomePage } from "@/pages/home";
@@ -7,6 +8,7 @@ import { ServicesPage } from "@/pages/services";
 const publicRoutes = [
   { path: "/", component: HomePage },
   { path: "/icons", component: IconList },
+  { path: "/about", component: AboutUsPage },
   { path: "/services", component: ServicesPage },
   { path: "/contact", component: ContactPage },
   { path: "/gallery", component: GalleryPage },
---
+++ src/pages/about_us/index.jsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout/Layout"
+
+export const AboutUsPage = () => {
+    return (
+        <Layout breadcrumbTitle={"Sobre Nosotros"}>
+            sobre nosotros
+        </Layout>
+    )
+}
